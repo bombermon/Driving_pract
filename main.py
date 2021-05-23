4154499a250c5935fb6e496aa3091a2817e4bd3e
Руслан <fazluevruslan@gmail.com>
2021-05-23 13:21:16 +0300

    4.2 done

--- main.py
+++ main.py
@@ -56,3 +56,21 @@ def task_4_1():
     dr.show()
     dr.clean()
 
+
+def task_4_2():
+    n = 10
+
+    first_line = list(itr.islice(dr.gen_rectangle(), n))
+    second_line = list(itr.islice(dr.gen_rectangle(), n))
+
+    first_line = map(dr.tr_rotate, first_line, [13]*n, [0.35]*n, [math.pi/4]*n)
+    second_line = map(dr.tr_rotate, second_line, [13] * n, [0.5] * n, [-math.pi / 4] * n)
+
+    first_line = map(dr.tr_translate, first_line, [0]*n, [5]*n)
+
+    dr.plot(first_line)
+    dr.plot(second_line)
+
+    dr.show()
+    dr.clean()
+
