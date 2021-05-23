156eda0ba74e45a7c91e08f0b0d82ce7332d5407
Руслан <fazluevruslan@gmail.com>
2021-05-23 14:02:09 +0300

    6.3 DONE

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ import math
     4
     
 6:
-    выберем
+    3 DONE
        
 
 """
@@ -42,12 +42,12 @@ def task_4_1():
     second_line = list(itr.islice(dr.gen_rectangle(), n))
     third_line = list(itr.islice(dr.gen_rectangle(), n))
 
-    first_line = map(dr.tr_rotate, first_line, [10]*n, [0]*n, [math.pi/4]*n)
-    second_line = map(dr.tr_rotate, second_line, [10] * n, [0] * n, [math.pi / 4] * n)
-    third_line = map(dr.tr_rotate, third_line, [10] * n, [0] * n, [math.pi / 4] * n)
+    first_line = list(map(dr.tr_rotate, first_line, [10]*n, [0]*n, [math.pi/4]*n))
+    second_line = list(map(dr.tr_rotate, second_line, [10] * n, [0] * n, [math.pi / 4] * n))
+    third_line = list(map(dr.tr_rotate, third_line, [10] * n, [0] * n, [math.pi / 4] * n))
 
-    first_line = map(dr.tr_translate, first_line, [0]*n, [5]*n)
-    third_line = map(dr.tr_translate, third_line, [0]*n, [-5]*n)
+    first_line = list(map(dr.tr_translate, first_line, [0]*n, [5]*n))
+    third_line = list(map(dr.tr_translate, third_line, [0]*n, [-5]*n))
 
     dr.plot(first_line)
     dr.plot(second_line)
@@ -63,10 +63,10 @@ def task_4_2():
     first_line = list(itr.islice(dr.gen_rectangle(), n))
     second_line = list(itr.islice(dr.gen_rectangle(), n))
 
-    first_line = map(dr.tr_rotate, first_line, [13]*n, [0.35]*n, [math.pi/4]*n)
-    second_line = map(dr.tr_rotate, second_line, [13] * n, [0.5] * n, [-math.pi / 4] * n)
+    first_line = list(map(dr.tr_rotate, first_line, [13]*n, [0.35]*n, [math.pi/4]*n))
+    second_line = list(map(dr.tr_rotate, second_line, [13] * n, [0.5] * n, [-math.pi / 4] * n))
 
-    first_line = map(dr.tr_translate, first_line, [0]*n, [5]*n)
+    first_line = list(map(dr.tr_translate, first_line, [0]*n, [5]*n))
 
     dr.plot(first_line)
     dr.plot(second_line)
@@ -81,7 +81,7 @@ def task_4_3():
     first_line = list(itr.islice(dr.gen_triangle(), n))
     second_line = list(itr.islice(dr.gen_triangle(), n))
 
-    second_line = map(dr.tr_symmetry, second_line, [0] * n, [1.1] * n, [1] * n, [1.1] * n)
+    second_line = list(map(dr.tr_symmetry, second_line, [0] * n, [1.1] * n, [1] * n, [1.1] * n))
 
     dr.plot(first_line)
     dr.plot(second_line)
@@ -89,3 +89,29 @@ def task_4_3():
     dr.show()
     dr.clean()
 
+
+def task_6_3():
+    n = 30
+
+    first_line = list(itr.islice(dr.gen_hexagon(), n))
+    second_line = list(itr.islice(dr.gen_hexagon(), n))
+
+    first_line = list(map(dr.tr_rotate, first_line, [20]*n, [0]*n, [math.pi / 150]*n))
+    second_line = list(map(dr.tr_rotate, second_line, [20] * n, [0] * n, [-math.pi / 150] * n))
+
+    first_line = list(map(dr.tr_translate, first_line, [0]*n, [-0.5]*n))
+
+    dr.plot(first_line)
+    dr.plot(second_line)
+
+    dr.show()
+    dr.clean()
+
+    for figure in first_line:
+        second_line = list(filter(lambda x: not dr.flt_polygon_angles_inside(figure, x), second_line))
+
+    dr.plot(first_line)
+    dr.plot(second_line)
+
+    dr.show()
+    dr.clean()
