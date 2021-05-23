851cfa03a65ea265ca8fd189c05ce5c2067afdbb
bombermon <npototun0@gmail.com>
2021-05-23 12:39:26 +0300

    2.4 undone

--- main.py
+++ main.py
@@ -38,10 +38,13 @@ import math
 
 """
 
-a = tuple(itertools.islice(dr.gen_triangle(), 3))
-a = tuple(map(dr.tr_symmetry, a, [0]*len(a), [0]*len(a), [1]*len(a), [1]*len(a)))
-c = [((0, 0), (0, 1), (1, 1)), ((2, 0), (2, 1), (3, 1))]
-b = tuple(filter(lambda x: dr.flt_angle_point(x, (0, 0)), c))
+a = tuple(itertools.islice(dr.gen_triangle(), 2))
+b =  tuple(itertools.islice(dr.gen_hexagon(), 3))
+c = tuple(itertools.islice(dr.gen_rectangle(), 2))
+#a = tuple(map(dr.tr_symmetry, a, [0]*len(a), [0]*len(a), [1]*len(a), [1]*len(a)))
+#c = [((0, 0), (0, 1), (1, 1)), ((2, 0), (2, 1), (3, 1))]
+#b = tuple(filter(lambda x: dr.flt_angle_point(x, (0, 0)), c))
 dr.plot(a)
 dr.plot(b)
+dr.plot(c)
 dr.show()
