5d85b43435d094b64ef42ea8f7561f8aa21c5e78
V <71629856+bombermon@users.noreply.github.com>
2021-05-26 17:20:44 +0300

    Delete test2.py

--- test2.py
+++
@@ -1,9 +0,0 @@
-import driving as dr
-import functools as ft
-from functools import reduce
-
-"""tirangle1 = ((0, 0), (0, 1), (1, 1))
-print(dr.flt_short_side(tirangle1, 0.3))"""
-figures = [((0, 1), (0, 1), (1, 1), (1, 0)), ((0, 0), (0, 1), (1, 1), (1, 0))]
-print(reduce(dr.agr_min_area, figures, 999))
-
